caffb462b5e4e22a5ca8d2a9490aab2c7e817a72
void2610 <izumisan2610@gmail.com>
2025-07-06 10:10:55 +0900

    PlayStyleのCustomDrawerを実装

--- Assets/Scripts/Editor/PlayStylePropertyDrawer.cs
+++ Assets/Scripts/Editor/PlayStylePropertyDrawer.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 using UnityEditor;
 
 /// <summary>
-/// CardAttribute enumのカスタムProperty Drawer
+/// PlayStyle enumのカスタムProperty Drawer
 /// Inspectorで日本語名を表示する
 /// </summary>
-[CustomPropertyDrawer(typeof(CardAttribute))]
-public class CardAttributePropertyDrawer : PropertyDrawer
+[CustomPropertyDrawer(typeof(PlayStyle))]
+public class PlayStylePropertyDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         EditorGUI.BeginProperty(position, label, property);
         
         // 選択肢を作成（日本語名を含む）
-        var enumValues = System.Enum.GetValues(typeof(CardAttribute));
+        var enumValues = System.Enum.GetValues(typeof(PlayStyle));
         var options = new string[enumValues.Length];
         
         for (var i = 0; i < enumValues.Length; i++)
         {
-            var value = (CardAttribute)enumValues.GetValue(i);
-            var japaneseName = value.ToJapaneseName();
+            var value = (PlayStyle)enumValues.GetValue(i);
+            var japaneseName = value.ToJapaneseString();
             options[i] = $"{value} ({japaneseName})";
         }
         
