5543db17c13f2eae2da6d843d13b20a4f0f626e7
void2610 <contact@void2610.dev>
2026-08-19 03:03:19 +0900

    test: LiminalScenario を Test Runner で回す PlayMode ランナーを追加

---
+++ Assets/Tests/PlayMode/NovelScenariosE2ETests.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using Void2610.LiminalPalette.TestSupport;
+
+/// <summary>
+/// "Novel/Scenario/" prefix の LiminalScenario を Unity Test Runner で回す薄いランナー
+/// シナリオを追加すれば自動で Test Runner に出現する
+/// </summary>
+public sealed class NovelScenariosE2ETests
+{
+    [UnityTest]
+    public IEnumerator Run([ValueSource(nameof(Paths))] string scenarioPath) => LiminalPaletteTestRunner.RunScenario(scenarioPath);
+
+    public static IEnumerable<string> Paths => LiminalPaletteTestRunner.GetScenariosWithPrefix("Novel/Scenario/");
+}
